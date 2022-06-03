f2d7de737448036f3eb5a655754d95c11ad8eed8
MeesalaAnkaiah <MeesalaAnkaiah@host.docker.internal>
2022-06-03 13:20:31 +0530

    Added the validations

--- EMS/src/main/java/com/intelliswift/EMS/DAOs/Employee.java
+++ EMS/src/main/java/com/intelliswift/EMS/DAOs/Employee.java
@@ -1,10 +1,13 @@
 package com.intelliswift.EMS.DAOs;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -23,10 +26,18 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+    @NotNull
     private String first_name;
+    @NotNull
     private String last_name;
+    @NotNull
     private String user_name;
+    @NotNull
+    @Email
+    @Column(name="email", unique=true)
     private String email;
+    @NotNull
+    @Column(name="password", unique=true)
     private String password;
 }
 
